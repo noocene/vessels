10d96b52bea58a84f5dc6a51689ec1a9932580ab
Jacob Swart <zenerboson@gmail.com>
2019-01-22 11:44:50 -0800

    Added "check" feature to support RLS

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(target_arch = "wasm32", target_arch = "asmjs", feature = "check"))]
 #[macro_use]
 extern crate stdweb;
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(target_arch = "wasm32", target_arch = "asmjs", feature = "check"))]
 #[macro_use]
 extern crate serde_derive;
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(target_arch = "wasm32", target_arch = "asmjs", feature = "check"))]
 #[macro_use]
 extern crate stdweb_derive;
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(target_arch = "wasm32", target_arch = "asmjs", feature = "check"))]
 extern crate weak_table;
 
 pub(crate) mod render;
@@ -15,7 +15,7 @@ mod targets;
 
 pub use crate::render::{Frame, Point, Rect, Renderer, Size};
 
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(target_arch = "wasm32", target_arch = "asmjs", feature = "check"))]
 pub fn initialize() -> impl Renderer {
     targets::web::WebGL2::new()
 }
