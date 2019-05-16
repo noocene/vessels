30bf1eb178d4c78cb1b9d6e09d25fb08d4a7497d
Codadillo <leoconr@nuevaschool.org>
2019-05-16 15:56:29 -0700

    Added primitive color match caching

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let gfx = graphics_2d::new();
     let mut root = gfx.frame();
     let mut squares = vec![];
-    for i in 0..20000 {
+    for i in 0..100_000 {
         squares.push({
             let mut object = root.add(Primitive::square(1.).fill(Color::rgba(0, 255, 0, 255).into()).finalize().into());
             object.apply_transform(Transform::default().with_position(i as f64));
