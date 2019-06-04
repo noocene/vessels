f62fde3769333f6a5f21d245ec4770dd069dd32b
Jacob Swart <zenerboson@gmail.com>
2019-06-04 09:33:14 -0700

    formatting

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -7,7 +7,7 @@ pub mod mesh;
 /// Provides client/server networking functionality.
 pub mod centralized;
 
-/// A bidirectional binary data channel
+/// A bidirectional binary data channel.
 pub trait DataChannel:
     Stream<Item = Vec<u8>, Error = Error> + Sink<SinkItem = Vec<u8>, SinkError = Error> + Send
 {
