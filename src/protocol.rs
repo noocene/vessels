5a913624a2f7c60bc47473bbb031f7466ecaf49d
Izzy Swart <zenerboson@gmail.com>
2019-07-30 22:49:10 -0700

    WIP future support

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,6 +1,7 @@
 use crate::executor;
 use crossbeam_channel::{unbounded, Receiver, Sender, TryRecvError};
-use futures::{task::AtomicTask, Async, AsyncSink, Future, Poll, Sink, StartSend, Stream};
+use futures::{task::AtomicTask, Async, AsyncSink, Poll, Sink, StartSend, Stream, IntoFuture};
+use futures::Future as Fut;
 use serde::{de::DeserializeOwned, Serialize};
 use std::sync::Arc;
 
@@ -79,21 +80,71 @@ impl<T: Serialize + DeserializeOwned> futures::Stream for Context<T> {
 pub trait Value {
     type Item: Serialize + DeserializeOwned + Send + 'static;
 
-    fn construct(context: Context<Self::Item>) -> Self
+    fn construct<C: Sink<SinkItem = Self::Item, SinkError = ()> + Stream<Item = Self::Item, Error = ()> + Send + 'static>(context: C) -> Self
     where
         Self: Sized;
-    fn deconstruct(self, context: Context<Self::Item>)
+    fn deconstruct<C: Sink<SinkItem = Self::Item, SinkError = ()> + Stream<Item = Self::Item, Error = ()> + Send + 'static>(self, context: C)
     where
         Self: Sized;
 }
 
-/*pub struct Future<T: Value, E: Value> {
-    item: PhantomData<T>,
-    error: PhantomData<E>,
+pub struct Future<T: Value, E: Value> {
+    future: Box<dyn futures::Future<Item = T, Error = E> + Send + 'static>,
 }
 
-impl<T: Value, E: Value> Value for Future<T, E> {
-    type Item = ();
+impl<T: Value, E: Value> Future<T, E> {
+    fn new<F: IntoFuture<Item = T, Error = E> + Send + 'static>(future: F) -> Self where F::Future: Send {
+        Future {
+            future: Box::new(future.into_future()),
+        }
+    }
+}
+
+fn future_with_ok<T: Value, E: Value>(ok: T::Item) -> Result<Result<T::Item, E::Item>, ()> {
+    Ok(Ok(ok))
+}
+
+fn future_with_err<T: Value, E: Value>(item: E::Item) -> Result<Result<T::Item, E::Item>, ()> {
+    Ok(Err(item))
+}
+
+impl<T: Value + Send + 'static, E: Value + Send + 'static> Value for Future<T, E> {
+    type Item = Result<T::Item, E::Item>;
+
+    fn construct<C: Sink<SinkItem = Self::Item, SinkError = ()> + Stream<Item = Self::Item, Error = ()> + Send + 'static>(context: C) -> Self where Self: Sized {
+        executor::spawn(context.for_each(|v| {println!("{}", serde_json::to_string(&v).unwrap()); Ok(())}));
+        Future {
+            future: Box::new(futures::future::done(Ok(T::construct(Context::new().0)))),
+        }
+    }
+
+    fn deconstruct<C: Sink<SinkItem = Self::Item, SinkError = ()> + Stream<Item = Self::Item, Error = ()> + Send + 'static>(self, context: C) where Self: Sized {
+        executor::spawn(self.future.then(|result| {
+            match result {
+                Ok(value) => {
+                    let ctx = context.map(|item| {
+                        if let Ok(item) = item {
+                            return item;
+                        } else {
+                            panic!("Invalid result in future stream");
+                        }
+                    }).with(future_with_ok::<T, E>);
+                    value.deconstruct(ctx);
+                }
+                Err(value) => {
+                    let ctx = context.map(|item| {
+                        if let Err(item) = item {
+                            return item;
+                        } else {
+                            panic!("Invalid result in future stream");
+                        }
+                    }).with(future_with_err::<T, E>);
+                    value.deconstruct(ctx);
+                }
+            }
+            Ok(())
+        }))
+    }
 }
 
 impl<T: Value, E: Value> futures::Future for Future<T, E> {
@@ -101,11 +152,11 @@ impl<T: Value, E: Value> futures::Future for Future<T, E> {
     type Error = E;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        Ok(Async::NotReady)
+        self.future.poll()
     }
 }
 
-pub struct Object<T: ?Sized> {
+/*pub struct Object<T: ?Sized> {
     inner: Box<T>,
 }
 
@@ -119,14 +170,14 @@ where
 {
     type Item = T;
 
-    fn construct(context: Context<Self::Item>) -> Self {
+    fn construct<C: Sink<SinkItem = Self::Item, SinkError = ()> + Stream<Item = Self::Item, Error = ()> + Send + 'static>(context: C) -> Self {
         if let Ok(v) = context.into_future().wait() {
             return v.0.unwrap();
         } else {
             panic!("panic in construction");
         }
     }
-    fn deconstruct(self, context: Context<Self::Item>)
+    fn deconstruct<C: Sink<SinkItem = Self::Item, SinkError = ()> + Stream<Item = Self::Item, Error = ()> + Send + 'static>(self, context: C)
     where
         Self: Sized,
     {
