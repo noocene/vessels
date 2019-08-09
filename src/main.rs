ec80631de65dccb26536b5c8cf663963d7c90c0c
Izzy Swart <zenerboson@gmail.com>
2019-08-09 14:32:42 -0700

    generic nonce providers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use vessels::{crypto::{primitives::SymmetricKey, self}, executor};
+use vessels::{crypto::primitives::{SymmetricKey, nonce_providers::Random}, executor};
 use futures::Future;
 
 #[macro_use]
 extern crate stdweb;
 
 fn main() {
-    executor::spawn(SymmetricKey::new().and_then(|key| {
+    executor::spawn(SymmetricKey::new().and_then(|key: Box<dyn SymmetricKey<Random>>| {
         key.encrypt("hello".as_bytes()).and_then(move |encrypted| {
             console!(log, &encrypted);
             key.decrypt(encrypted.as_slice()).and_then(|decrypted| {
