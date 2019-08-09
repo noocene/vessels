d23ba809f0a3bfe0698403d244e51ba24137ff5e
Izzy Swart <zenerboson@gmail.com>
2019-08-09 13:07:45 -0700

    use fixed-size arrays for key data

--- src/crypto/primitives.rs
+++ src/crypto/primitives.rs
@@ -4,19 +4,26 @@ use futures::Future;
 use serde::{de::DeserializeOwned, Serialize, ser::Serializer, de::{Visitor, Deserialize, Deserializer}};
 use std::fmt;
 
-/// Backed by AES-128 in GCM on all platforms, interoperable. AES-256 is not used as the probability of success for a brute-force attack on AES-128 is already far more slim than necessary and the AES-256 key schedule is less well designed.
+/// Backed by AES-128 in GCM on all platforms, interoperable.
+/// 
+/// AES-256 is not used as the probability of success for a brute-force attack on AES-128 is already far more slim than necessary and the AES-256 key schedule is less well designed.
 pub trait SymmetricKey: Send {
+    /// Encrypts the provided data.
     fn encrypt(&self, data: &'_ [u8]) -> Box<dyn Future<Item = Vec<u8>, Error = Error> + Send>;
+    /// Decrypts the provided data.
     fn decrypt(&self, data: &'_ [u8]) -> Box<dyn Future<Item = Vec<u8>, Error = Error> + Send>;
-    fn as_bytes(&self) -> Box<dyn Future<Item = Vec<u8>, Error = Error>>;
+    /// Exports a key as a raw 128-bit byte array.
+    fn as_bytes(&self) -> Box<dyn Future<Item = [u8; 16], Error = Error>>;
 }
 
 impl dyn SymmetricKey {
+    /// Constructs a new random key from a secure source of entropy.
     pub fn new() -> impl Future<Item = Box<dyn SymmetricKey + 'static>, Error = Error> {
         #[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
         targets::web::crypto::primitives::AESKey::new()
     }
-    pub fn from_bytes(data: &'_ [u8]) -> impl Future<Item = Box<dyn SymmetricKey + 'static>, Error = Error> {
+    /// Imports a key from a raw 128-bit byte array.
+    pub fn from_bytes(data: [u8; 16]) -> impl Future<Item = Box<dyn SymmetricKey + 'static>, Error = Error> {
         #[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
         targets::web::crypto::primitives::AESKey::from_bytes(data)
     }
@@ -39,8 +46,10 @@ impl<'de> Visitor<'de> for SymmetricKeyVisitor {
     fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result { 
         formatter.write_str("raw binary AES key data")
     }
-    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E> {
-        Ok(SymmetricKey::from_bytes(v).wait().unwrap())
+    fn visit_bytes<E>(self, v: &'_ [u8]) -> Result<Self::Value, E> {
+        let mut a: [u8; 16] = Default::default();
+        a.copy_from_slice(v);
+        Ok(SymmetricKey::from_bytes(a).wait().unwrap())
     }
 }
 
