4c2edaa2650332dc64c4094868a07745ec293002
Izzy Swart <zenerboson@gmail.com>
2019-08-09 17:45:42 -0700

    documentation for signatures

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use futures::Future;
 use vessels::{
-    crypto::primitives::{nonce_providers::Random, SigningKeyPair, SigningKey, VerifyingKey},
+    crypto::primitives::{nonce_providers::Random, SigningKeyPair},
     executor,
 };
 
@@ -9,13 +9,19 @@ extern crate stdweb;
 
 fn main() {
     executor::run(
-        SigningKeyPair::new().and_then(|(private_key, public_key)| {
-            private_key.sign("hello".as_bytes()).and_then(move |signature| {
-                public_key.verify("hello".as_bytes(), &signature).and_then(|result| {
-                    console!(log, result);
-                    Ok(())
-                })
+        SigningKeyPair::new()
+            .and_then(|(private_key, public_key)| {
+                private_key
+                    .sign("hello".as_bytes())
+                    .and_then(move |signature| {
+                        public_key
+                            .verify("hello".as_bytes(), &signature)
+                            .and_then(|result| {
+                                console!(log, result);
+                                Ok(())
+                            })
+                    })
             })
-        }).then(|_| Ok(()))
+            .then(|_| Ok(())),
     );
 }
