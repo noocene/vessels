9e5cb826e1f41e577f62d5d2f00daba36d9e8c3a
Izzy Swart <zenerboson@gmail.com>
2019-08-26 20:34:41 -0700

    formatting pass

--- src/targets/native/graphics/pure2d.rs
+++ src/targets/native/graphics/pure2d.rs
@@ -726,7 +726,7 @@ impl Frame for CairoFrame {
                     let context = state.context.lock().unwrap();
                     context.restore();
                     context.save();
-                    context.scale(1.0/state.pixel_ratio, 1.0/state.pixel_ratio);
+                    context.scale(1. / state.pixel_ratio, 1. / state.pixel_ratio);
                     context.transform(Matrix {
                         xx: matrix[0],
                         yx: matrix[2],
@@ -807,21 +807,27 @@ impl CairoObject {
                     (corners.1.x - corners.0.x).abs(),
                     (corners.1.y - corners.0.y).abs(),
                 ));
-                let base_surface =
-                    ImageSurface::create(Format::ARgb32, size.x as i32 * pixel_ratio as i32, size.y as i32 * pixel_ratio as i32).unwrap();
+                let base_surface = ImageSurface::create(
+                    Format::ARgb32,
+                    size.x as i32 * pixel_ratio as i32,
+                    size.y as i32 * pixel_ratio as i32,
+                )
+                .unwrap();
                 let base_context = CairoContext(cairo::Context::new(&base_surface));
                 base_context.scale(pixel_ratio, pixel_ratio);
                 update_clip(&base_context, &path.clone().with_origin(corners.0));
                 for shadow in &path.shadows {
-                    let mut shadow = shadow.clone();
                     let spread = shadow.spread * 2.;
                     let size = path.bounds().size;
                     let scale = (size + spread) / size;
                     let segments = path.segments.iter();
                     let new_size = size + spread + (shadow.blur * 8.);
-                    let surface =
-                        ImageSurface::create(Format::ARgb32, new_size.x as i32 * pixel_ratio as i32, new_size.y as i32 * pixel_ratio as i32)
-                            .unwrap();
+                    let surface = ImageSurface::create(
+                        Format::ARgb32,
+                        new_size.x as i32 * pixel_ratio as i32,
+                        new_size.y as i32 * pixel_ratio as i32,
+                    )
+                    .unwrap();
                     let context = CairoContext(cairo::Context::new(&surface));
                     let scale_offset = (size - new_size) / 2.;
                     context.scale(scale.x * pixel_ratio, scale.y * pixel_ratio);
