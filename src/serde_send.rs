856267a5c3cf6f881dc37e2efc559de621db8b83
Izzy Swart <zenerboson@gmail.com>
2019-10-27 15:58:22 -0700

    remove serde_send

--- src/serde_send.rs
+++
@@ -1,7 +0,0 @@
-use serde::de::Deserializer;
-
-pub trait DeserializeSeedBounded<'de, D: Deserializer<'de>>: Sized {
-    type Kind;
-
-    fn deserialize(self, deserializer: D) -> Result<SelfValue, D::Error>;
-}
