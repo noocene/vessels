bbd2718ebdc0c4ddc76da5eecb6aa4c0bbf1d3e4
Izzy Swart <zenerboson@gmail.com>
2019-10-27 11:50:51 -0700

    arbitrary serde Kind

---
+++ src/kind/serde.rs
@@ -0,0 +1,53 @@
+use serde::{de::DeserializeOwned, Serialize};
+
+use futures::Future;
+
+use crate::{channel::Channel, Kind};
+
+use std::ops::Deref;
+
+pub struct Serde<T: Serialize + DeserializeOwned + Send + 'static>(pub T);
+
+impl<T: Serialize + DeserializeOwned + Send + 'static> Serde<T> {
+    pub fn new(item: T) -> Self {
+        Serde(item)
+    }
+}
+
+impl<T: Serialize + DeserializeOwned + Send + 'static> Deref for Serde<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T: Serialize + DeserializeOwned + Send + 'static> From<T> for Serde<T> {
+    fn from(item: T) -> Self {
+        Serde(item)
+    }
+}
+
+impl<T: Serialize + DeserializeOwned + Send + 'static> Kind for Serde<T> {
+    type ConstructItem = T;
+    type ConstructFuture = Box<dyn Future<Item = Serde<T>, Error = ()> + Send + 'static>;
+    type DeconstructItem = ();
+    type DeconstructFuture = Box<dyn Future<Item = (), Error = ()> + Send + 'static>;
+
+    fn deconstruct<C: Channel<Self::DeconstructItem, Self::ConstructItem>>(
+        self,
+        channel: C,
+    ) -> Self::DeconstructFuture {
+        Box::new(channel.send(self.0).then(|_| Ok(())))
+    }
+    fn construct<C: Channel<Self::ConstructItem, Self::DeconstructItem>>(
+        channel: C,
+    ) -> Self::ConstructFuture {
+        Box::new(
+            channel
+                .into_future()
+                .map_err(|e| panic!(e))
+                .map(|v| Serde(v.0.unwrap())),
+        )
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -10,22 +10,9 @@ pub mod format;
 pub mod kind;
 
 use erased_serde::Serialize as ErasedSerialize;
-use futures::{
-    future::{ok, FutureResult},
-    Future,
-};
+use futures::Future;
 use serde::{de::DeserializeOwned, Serialize};
-use std::{
-    any::Any,
-    ffi::{CString, OsString},
-    marker::PhantomData,
-    net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6},
-    num::{
-        NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroIsize, NonZeroU16, NonZeroU32,
-        NonZeroU64, NonZeroU8, NonZeroUsize,
-    },
-    time::{Duration, SystemTime},
-};
+use std::any::Any;
 
 pub trait Kind: Sized + Send + 'static {
     type ConstructItem: Serialize + DeserializeOwned + Send + 'static;
@@ -43,75 +30,6 @@ pub trait Kind: Sized + Send + 'static {
         channel: C,
     ) -> Self::DeconstructFuture;
 }
-
-impl Kind for () {
-    type ConstructItem = ();
-    type DeconstructItem = ();
-    type ConstructFuture = FutureResult<(), ()>;
-    type DeconstructFuture = FutureResult<(), ()>;
-
-    fn deconstruct<C: Channel<Self::DeconstructItem, Self::ConstructItem>>(
-        self,
-        _: C,
-    ) -> Self::DeconstructFuture {
-        ok(())
-    }
-    fn construct<C: Channel<Self::ConstructItem, Self::DeconstructItem>>(
-        _: C,
-    ) -> Self::ConstructFuture {
-        ok(())
-    }
-}
-
-impl<T: Send + 'static> Kind for PhantomData<T> {
-    type ConstructItem = ();
-    type ConstructFuture = FutureResult<PhantomData<T>, ()>;
-    type DeconstructItem = ();
-    type DeconstructFuture = FutureResult<(), ()>;
-
-    fn deconstruct<C: Channel<Self::DeconstructItem, Self::ConstructItem>>(
-        self,
-        _: C,
-    ) -> Self::DeconstructFuture {
-        ok(())
-    }
-    fn construct<C: Channel<Self::ConstructItem, Self::DeconstructItem>>(
-        _: C,
-    ) -> Self::ConstructFuture {
-        ok(PhantomData)
-    }
-}
-
-macro_rules! primitive_impl {
-    ($($ty:ident)+) => {$(
-        impl Kind for $ty {
-            type ConstructItem = $ty;
-            type ConstructFuture = Box<dyn Future<Item = $ty, Error = ()> + Send + 'static>;
-            type DeconstructItem = ();
-            type DeconstructFuture = Box<dyn Future<Item = (), Error = ()> + Send + 'static>;
-
-            fn deconstruct<C: Channel<Self::DeconstructItem, Self::ConstructItem>>(
-                self,
-                channel: C,
-            ) -> Self::DeconstructFuture {
-                Box::new(channel.send(self).then(|_| Ok(())))
-            }
-            fn construct<C: Channel<Self::ConstructItem, Self::DeconstructItem>>(
-                channel: C,
-            ) -> Self::ConstructFuture
-            {
-                Box::new(
-                    channel
-                        .into_future()
-                        .map_err(|e| panic!(e))
-                        .map(|v| v.0.unwrap()),
-                )
-            }
-        }
-    )+};
-}
-
-primitive_impl!(bool isize i8 i16 i32 i64 usize u8 u16 u32 u64 f32 f64 char CString String Ipv4Addr SocketAddrV4 SocketAddrV6 SocketAddr SystemTime OsString Ipv6Addr Duration NonZeroU8 NonZeroU16 NonZeroU32 NonZeroU64 NonZeroUsize NonZeroI8 NonZeroI16 NonZeroI32 NonZeroI64 NonZeroIsize);
 
 pub(crate) trait SerdeAny: erased_serde::Serialize + mopa::Any + Send {
     fn as_any(self) -> Box<dyn Any>
