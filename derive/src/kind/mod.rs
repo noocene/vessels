b8984efba2ae6f207c07a5466532391998cf797a
Izzy Swart <zenerboson@gmail.com>
2019-10-27 14:02:29 -0700

    correct hygiene on kind macro

--- derive/src/kind/mod.rs
+++ derive/src/kind/mod.rs
@@ -1,27 +1,26 @@
 use proc_macro2::{Span, TokenStream};
 use proc_macro_error::*;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse2, Item, Type};
 
 pub(crate) fn kind(attr: TokenStream, mut item: TokenStream) -> TokenStream {
     let mut items = proc_macro2::TokenStream::new();
+    let ident = match parse2::<Item>(item.clone()).unwrap_or_else(|_| {
+        abort!(
+            Span::call_site(),
+            "kind must be generated from a struct or enu,"
+        )
+    }) {
+        Item::Struct(item) => item.ident,
+        Item::Enum(item) => item.ident,
+        _ => abort!(
+            Span::call_site(),
+            "kind must be generated from a struct or enu,"
+        ),
+    };
     items.extend(
         parse2::<Type>(attr)
             .map(|ty| -> TokenStream {
-                let item = parse2::<Item>(item.clone()).unwrap_or_else(|_| {
-                    abort!(
-                        Span::call_site(),
-                        "kind must be generated from a struct or enu,"
-                    )
-                });
-                let ident = match item {
-                    Item::Struct(item) => item.ident,
-                    Item::Enum(item) => item.ident,
-                    _ => abort!(
-                        Span::call_site(),
-                        "kind must be generated from a struct or enu,"
-                    ),
-                };
                 (quote! {
                     impl Kind for #ident {
                         type ConstructItem = <<#ident as AsKind<#ty>>::Kind as Kind>::ConstructItem;
@@ -46,8 +45,10 @@ pub(crate) fn kind(attr: TokenStream, mut item: TokenStream) -> TokenStream {
             })
             .unwrap_or_else(|_| abort!(Span::call_site(), "the target Kind must be specified")),
     );
+    let ident = format_ident!("_IMPLEMENT_KIND_FOR_{}", ident);
     item.extend(quote! {
-        const _IMPLEMENT_KIND_FOR_TYPE: () = {
+        #[allow(non_upper_case_globals)]
+        const #ident: () = {
             #items
         };
     });
