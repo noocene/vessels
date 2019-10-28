9f36bf0a3cd16b046684b0353a9cbf11d4a4152a
Izzy Swart <zenerboson@gmail.com>
2019-10-28 11:29:33 -0700

    stylistic changes to collections macro

--- src/kind/collections.rs
+++ src/kind/collections.rs
@@ -8,54 +8,60 @@ use std::hash::Hash;
 use futures::{future::join_all, Future};
 
 macro_rules! iterator_impl {
-    ($($ty:ident $(where $first_bound:tt $(+ $more_bounds:tt)*)?),+) => {$(
-        impl<T> Kind for $ty<T>
-where
-    T: Kind $(+ $first_bound $(+ $more_bounds)*)?
-{
-    type ConstructItem = Vec<ForkHandle>;
-    type ConstructFuture = Box<dyn Future<Item = Self, Error = ()> + Send>;
-    type DeconstructItem = ();
-    type DeconstructFuture = Box<dyn Future<Item = (), Error = ()> + Send>;
-    fn deconstruct<C: Channel<Self::DeconstructItem, Self::ConstructItem>>(
-        self,
-        channel: C,
-    ) -> Self::DeconstructFuture {
-        Box::new(
-            join_all(
-                self.into_iter()
-                    .map(|entry| channel.fork::<T>(entry))
-                    .collect::<Vec<Box<dyn Future<Item = ForkHandle, Error = ()> + Send>>>(),
-            )
-            .map_err(|_| panic!("lol"))
-            .and_then(|handles| {
-                channel
-                    .send(handles)
-                    .and_then(|_| Ok(()))
-                    .map_err(|_| panic!())
-            }),
-        )
-    }
-    fn construct<C: Channel<Self::ConstructItem, Self::DeconstructItem>>(
-        channel: C,
-    ) -> Self::ConstructFuture {
-        Box::new(
-            channel
-                .into_future()
-                .map_err(|_| panic!("lol"))
-                .and_then(|(item, channel)| {
+    ($($ty:ident < T $(: $tbound1:ident $(+ $tbound2:ident)*)* $(, $typaram:ident : $bound:ident)* >),+) => {$(
+        impl<T $(, $typaram)*> Kind for $ty<T $(, $typaram)*>
+            where T: Kind + Send + 'static $(+ $tbound1 $(+ $tbound2)*)*, $($typaram: $bound,)*
+        {
+            type ConstructItem = Vec<ForkHandle>;
+            type ConstructFuture = Box<dyn Future<Item = Self, Error = ()> + Send>;
+            type DeconstructItem = ();
+            type DeconstructFuture = Box<dyn Future<Item = (), Error = ()> + Send>;
+            fn deconstruct<C: Channel<Self::DeconstructItem, Self::ConstructItem>>(
+                self,
+                channel: C,
+            ) -> Self::DeconstructFuture {
+                Box::new(
                     join_all(
-                        item.unwrap()
-                            .into_iter()
-                            .map(|entry| channel.get_fork::<T>(entry))
-                            .collect::<Vec<Box<dyn Future<Item = T, Error = ()> + Send>>>(),
+                        self.into_iter()
+                            .map(|entry| channel.fork::<T>(entry))
+                            .collect::<Vec<Box<dyn Future<Item = ForkHandle, Error = ()> + Send>>>(),
                     )
-                    .map(|collection_as_vec| collection_as_vec.into_iter().collect())
-                }),
-        )
-    }
-}
+                    .map_err(|_| panic!("lol"))
+                    .and_then(|handles| {
+                        channel
+                            .send(handles)
+                            .and_then(|_| Ok(()))
+                            .map_err(|_| panic!())
+                    }),
+                )
+            }
+            fn construct<C: Channel<Self::ConstructItem, Self::DeconstructItem>>(
+                channel: C,
+            ) -> Self::ConstructFuture {
+                Box::new(
+                    channel
+                        .into_future()
+                        .map_err(|_| panic!("lol"))
+                        .and_then(|(item, channel)| {
+                            join_all(
+                                item.unwrap()
+                                    .into_iter()
+                                    .map(|entry| channel.get_fork::<T>(entry))
+                                    .collect::<Vec<Box<dyn Future<Item = T, Error = ()> + Send>>>(),
+                            )
+                            .map(|collection_as_vec| collection_as_vec.into_iter().collect())
+                        }),
+                )
+            }
+        }
     )+};
 }
 
-iterator_impl!(BinaryHeap where Ord, BTreeSet where Ord, HashSet where Hash + Eq, LinkedList, Vec, VecDeque);
+iterator_impl!(
+    BinaryHeap<T: Ord>,
+    BTreeSet<T: Ord>,
+    HashSet<T: Hash + Eq>,
+    LinkedList<T>,
+    Vec<T>,
+    VecDeque<T>
+);
