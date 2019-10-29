0aaad13ca805ad895235a56b2b9b9fc33219e9b5
Izzy Swart <zenerboson@gmail.com>
2019-10-28 23:22:46 -0700

    remove recursive select for out_channel

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use serde::{Deserialize, Serialize};
 
 use futures::{executor::ThreadPool, future::ready, FutureExt, StreamExt, TryFutureExt};
 
-#[derive(Serialize, Deserialize, Debug, Kind)]
+#[derive(Serialize, Deserialize, Debug, Kind, Clone)]
 #[kind(using::Serde)]
 enum TestEnum {
     Yes(u32),
@@ -17,11 +17,7 @@ enum TestEnum {
 }
 
 fn main() {
-    let meme: [TestEnum; 3] = [
-        TestEnum::No("ok this is epic".to_owned()),
-        TestEnum::No("ok this is unepic".to_owned()),
-        TestEnum::Yes(69),
-    ];
+    let meme: Vec<TestEnum> = vec![TestEnum::No("ok this is epic".to_owned()); 1000];
     ThreadPool::new().unwrap().run(
         meme.on_to::<IdChannel>()
             .map(Json::encode)
@@ -29,7 +25,7 @@ fn main() {
             .map(Json::decode::<IdChannel>)
             .flatten()
             .unwrap_or_else(|e| panic!(e))
-            .then(|item: [TestEnum; 3]| {
+            .then(|item: Vec<TestEnum>| {
                 println!("{:?}", item);
                 ready(())
             }),
