345b85bc35a6fc4f6fc0bccb6da3e78c0fc2d8b6
Izzy Swart <zenerboson@gmail.com>
2019-10-30 15:34:46 -0700

    add wrapping iterator kind

---
+++ src/kind/iterator.rs
@@ -0,0 +1,150 @@
+use futures::{
+    future::{join_all, BoxFuture},
+    SinkExt, StreamExt, TryFutureExt,
+};
+
+use crate::{
+    channel::{Channel, ForkHandle},
+    ConstructResult, DeconstructResult, Kind,
+};
+
+use super::{using, AsKind};
+
+use std::{iter::FromIterator, ops::Deref};
+
+#[derive(Clone, Debug, Copy, Hash, Eq, Ord, PartialOrd, PartialEq, Default)]
+pub struct Iterator<T: Send + IntoIterator + FromIterator<<T as IntoIterator>::Item> + 'static>(
+    pub T,
+)
+where
+    <T as IntoIterator>::Item: Kind,
+    T::IntoIter: Send;
+
+impl<T: Send + IntoIterator + FromIterator<<T as IntoIterator>::Item> + 'static> Iterator<T>
+where
+    <T as IntoIterator>::Item: Kind,
+    T::IntoIter: Send,
+{
+    pub fn new(item: T) -> Self {
+        Iterator(item)
+    }
+}
+
+impl<T: Send + IntoIterator + FromIterator<<T as IntoIterator>::Item> + 'static> Deref
+    for Iterator<T>
+where
+    <T as IntoIterator>::Item: Kind,
+    T::IntoIter: Send,
+{
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T: Send + IntoIterator + FromIterator<<T as IntoIterator>::Item> + 'static> From<T>
+    for Iterator<T>
+where
+    <T as IntoIterator>::Item: Kind,
+    T::IntoIter: Send,
+{
+    fn from(item: T) -> Self {
+        Iterator(item)
+    }
+}
+
+impl<T: Send + IntoIterator + FromIterator<<T as IntoIterator>::Item> + 'static>
+    FromIterator<<T as IntoIterator>::Item> for Iterator<T>
+where
+    <T as IntoIterator>::Item: Kind,
+    T::IntoIter: Send,
+{
+    fn from_iter<U>(iter: U) -> Self
+    where
+        U: IntoIterator<Item = <T as IntoIterator>::Item>,
+    {
+        Iterator(iter.into_iter().collect())
+    }
+}
+
+impl<T: Send + IntoIterator + FromIterator<<T as IntoIterator>::Item> + 'static>
+    AsKind<using::Iterator> for T
+where
+    <T as IntoIterator>::Item: Kind,
+    T::IntoIter: Send,
+{
+    type Kind = Iterator<T>;
+    type ConstructFuture = BoxFuture<'static, Result<T, <Iterator<T> as Kind>::ConstructError>>;
+
+    fn into_kind(self) -> Iterator<T> {
+        Iterator(self)
+    }
+    fn from_kind(future: <Iterator<T> as Kind>::ConstructFuture) -> Self::ConstructFuture {
+        Box::pin(future.map_ok(|item| item.0))
+    }
+}
+
+impl<T: Send + IntoIterator + FromIterator<<T as IntoIterator>::Item> + 'static> IntoIterator
+    for Iterator<T>
+where
+    <T as IntoIterator>::Item: Kind,
+    T::IntoIter: Send,
+{
+    type Item = <T as IntoIterator>::Item;
+    type IntoIter = <T as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl<T: Send + IntoIterator + FromIterator<<T as IntoIterator>::Item> + 'static> Kind
+    for Iterator<T>
+where
+    <T as IntoIterator>::Item: Kind,
+    T::IntoIter: Send,
+{
+    type ConstructItem = Vec<ForkHandle>;
+    type ConstructError = ();
+    type ConstructFuture = BoxFuture<'static, ConstructResult<Self>>;
+    type DeconstructItem = ();
+    type DeconstructError = ();
+    type DeconstructFuture = BoxFuture<'static, DeconstructResult<Self>>;
+
+    fn deconstruct<C: Channel<Self::DeconstructItem, Self::ConstructItem>>(
+        self,
+        mut channel: C,
+    ) -> Self::DeconstructFuture {
+        Box::pin(async move {
+            channel
+                .send(
+                    join_all(self.0.into_iter().map(|entry| {
+                        channel
+                            .fork::<<T as IntoIterator>::Item>(entry)
+                            .unwrap_or_else(|_| panic!())
+                    }))
+                    .await,
+                )
+                .await
+                .map_err(|_| panic!())
+        })
+    }
+    fn construct<C: Channel<Self::ConstructItem, Self::DeconstructItem>>(
+        mut channel: C,
+    ) -> Self::ConstructFuture {
+        Box::pin(async move {
+            let handles = channel.next().await.unwrap();
+            Ok(Iterator(
+                join_all(handles.into_iter().map(|entry| {
+                    channel
+                        .get_fork::<<T as IntoIterator>::Item>(entry)
+                        .unwrap_or_else(|_| panic!())
+                }))
+                .await
+                .into_iter()
+                .collect(),
+            ))
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use kinddev::{
     channel::{Channel, IdChannel},
     format::{ApplyDecode, ApplyEncode, Json},
-    kind::{using, AsKind},
+    kind::{using, AsKind,Iterator},
     Kind, OnTo,
 };
 
 use serde::{Deserialize, Serialize};
 
-use futures::{executor::ThreadPool, future::BoxFuture};
+use futures::{executor::ThreadPool};
 
 #[derive(Serialize, Deserialize, Debug, Kind, Clone)]
 #[kind(using::Serde)]
@@ -16,13 +16,11 @@ enum TestEnum {
     No(String),
 }
 
-type Adder = Box<dyn FnOnce(u32, u32) -> BoxFuture<'static, u32> + Send + Sync>;
-
 fn main() {
-    let func: Adder = Box::new(|a, b| Box::pin(async move { a + b }));
+    let func = Iterator(vec!["test".to_owned(); 10]);
     ThreadPool::new().unwrap().run(async move {
         let encoded = func.on_to::<IdChannel>().await.encode::<Json>();
-        let add: Adder = encoded.decode::<IdChannel, Json>().await.unwrap();
-        println!("{}", add(20, 35).await);
+        let decoded: Iterator<Vec<String>> = encoded.decode::<IdChannel, Json>().await.unwrap();
+        println!("{:?}", decoded);
     })
 }
