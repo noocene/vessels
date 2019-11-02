606532170f24a78616ba548af8f941d4cef84385
Izzy Swart <zenerboson@gmail.com>
2019-11-01 23:09:37 -0700

    derive macro without generics support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use vessels::object;
+use vessels::{Kind, channel::IdChannel, format::{Json, ApplyEncode, ApplyDecode}, OnTo, kind::Iterator};
 
-#[object]
-pub trait Test: Send {
-    fn test(&self, string: String) -> u32;
-    fn other_test(&mut self, test: u32);
+use futures::{executor::ThreadPool};
+
+#[derive(Kind, Debug)]
+pub enum Test {
+    Item(Iterator<Vec<u32>>, u64),
+    StructStyle { test: u32 },
+    Other(String),
+    Empty,
 }
 
-fn main() {}
+fn main() {
+    let func = Test::Item(Iterator(vec![4, 5]), 4);
+    ThreadPool::new().unwrap().run(async move {
+        let encoded = func.on_to::<IdChannel>().await.encode::<Json>();
+        let decoded: Test = encoded.decode::<IdChannel, Json>().await.unwrap();
+        println!("{:?}", decoded);
+    })
+}
