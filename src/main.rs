76d2e4ca167ba879061838571b1c9c6b917c46d5
Izzy Swart <zenerboson@gmail.com>
2019-11-02 12:43:19 -0700

    add Send bounds in object implementation

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,8 @@
-use vessels::{
-    channel::IdChannel,
-    format::{ApplyDecode, ApplyEncode, Json},
-    kind::using,
-    Kind, OnTo,
-};
+use vessels::object;
 
-use futures::executor::ThreadPool;
-
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct NotKind<T>(T);
-
-#[derive(Kind, Debug)]
-pub enum Test<T> {
-    Test,
-    Two(u32, String),
-    Other {
-        #[kind(using::Serde)]
-        test: NotKind<T> 
-    },
+#[object]
+trait ObjectTest {
+    fn test(&self, hello: String) -> u32;
 }
 
-fn main() {
-    let test = Test::Other { test: NotKind(0u32) };
-    ThreadPool::new().unwrap().run(async move {
-        let encoded = test.on_to::<IdChannel>().await.encode::<Json>();
-        let decoded: Test<u32> = encoded.decode::<IdChannel, Json>().await.unwrap();
-        println!("{:?}", decoded);
-    })
-}
+fn main() {}
