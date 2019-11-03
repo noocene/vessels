3e4553c6c02b9c17c09bc681e60d949105a48a68
Izzy Swart <zenerboson@gmail.com>
2019-11-02 18:48:03 -0700

    document Kind

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,66 @@ use failure::Fail;
 use futures::Future;
 use serde::{de::DeserializeOwned, Serialize};
 
-pub use derive::{object, Kind};
+pub use derive::object;
+
+/// Macro that automatically generates an implementation of `Kind` for a struct or enum.
+///
+/// This macro has a number of modes of operation.
+/// First, it may be used in a fashion equivalent to the manner of operation of standard library derive macros.
+/// ```
+/// use vessels::Kind;
+///
+/// #[derive(Kind)]
+/// struct Person<T> {
+///     name: T,
+///     say_hello: Box<dyn Fn() -> T + Sync + Send>,
+/// }
+/// ```
+/// This will generate an implementation of `Kind` for the annotated type given an extant implementation of `Kind`
+/// for each field of that type. There is further nuance to this mode of operation, but to explain it is best to first
+/// demonstrate the other primary manner of operation.
+/// ```
+/// use vessels::{Kind, kind::using};
+/// use serde::{Serialize, Deserialize}
+///
+/// #[derive(Serialize, Deserialize)]
+/// struct NotKind;
+///
+/// #[derive(Serialize, Deserialize, Kind)]
+/// #[kind(using::Serde)]
+/// struct Person {
+///     name: String,
+///     data: NotKind,
+/// }
+/// ```
+/// This will generate an implementation of `Kind` for the annotated type despite `NotKind` lacking a valid implementation.
+/// The types, provided in `vessels::kind::using`, that provide `AsKind` trait implementations, allow for the use of an
+/// alternative bijection for structs and enums that implement some certain traits permitting such a thing. To finally attend
+/// to the additional mode of operation mentioned earlier, these `#[kind()]` annotations may be used with the initially discussed
+/// syntax.
+/// ```
+/// #[derive(Kind)]
+/// struct Person {
+///     name: String,
+///     #[kind(using::Serde)]
+///     data: NotKind,
+/// }
+/// ```
+/// Annotating a field of a struct or enum with `#[kind()]`, if the type provided in the attribute annotation is a valid `AsKind`
+/// for the type of that field, will cause the overarching derivation to use that type as a wrapper to produce a valid `Kind` bijection.
+/// All of the described behavior also functions for arbitrary generic parameters and when used in enums with both named and unnamed fields.
+/// ```
+/// #[derive(Kind)]
+/// enum Entity<T: Kind> {
+///     Person {
+///         name: String,
+///         #[kind(using::Serde)]
+///         data: NotKind,
+///     },
+///     UnnamedFields(#[kind(using::Serde)] NotKind, T)
+/// }
+/// ```
+pub use derive::Kind;
 
 #[doc(hidden)]
 pub use futures;
@@ -23,28 +82,63 @@ pub use serde;
 #[doc(hidden)]
 pub use void;
 
+/// The result of reconstructing a Kind.
 pub type ConstructResult<K> = Result<K, <K as Kind>::ConstructError>;
+/// The result of deconstructing a Kind.
 pub type DeconstructResult<K> = Result<(), <K as Kind>::DeconstructError>;
 
+/// A type with a bijection to over-the-wire data.
+///
+/// Kind is an advanced distributed object or RPC system that permits the over-the-wire serialization
+/// and deserialization of an implicitly flattened version of the complex nested structures required
+/// to produce a full type-level isomorphic representation of arbitrary composed data types.
+///
+/// Vessels provides `Kind` implementations for many primitive types from the standard library as
+/// well as futures, streams, a variety of boxed function types, and more.
+///
+/// Vessels also provides a derive macro that automatically generates `Kind` implementations for
+/// structs and enums in addition to the [object] macro for generating `Kind` implementations for
+/// trait objects of user-defined traits.
+///
+/// Authors of third-party crates are encouraged to derive or implement Kind or Kind providers for
+/// types their crates expose that might be useful over some form of wire boundary, be it network, IPC,
+/// or any other similar transport.
 pub trait Kind: Sized + Send + 'static {
+    /// The item transmitted over the network **to** the construction task
+    /// from deconstruction.
     type ConstructItem: Serialize + DeserializeOwned + Send + Sync + Unpin + 'static;
+    /// The failure condition of constructing a concrete type from communicated data.
     type ConstructError: Fail;
+    /// The concrete future type returned by the construction process.
     type ConstructFuture: Future<Output = ConstructResult<Self>> + Send + 'static;
 
+    /// Constructs the `Kind` from the provided channel. This method should return
+    /// immediately and, if necessary, move `channel` into some shim structure,
+    /// async block, or other owned state specified by `ConstructFuture`.
     fn construct<C: Channel<Self::ConstructItem, Self::DeconstructItem>>(
         channel: C,
     ) -> Self::ConstructFuture;
 
+    /// The item transmitted over the network **from** the construction task
+    /// to deconstruction.
     type DeconstructItem: Serialize + DeserializeOwned + Send + Sync + Unpin + 'static;
+    /// The failure condition of constructing a concrete type from communicated data.
     type DeconstructError: Fail;
+    /// The concrete future type returned by the deconstruction process. This is
+    /// used to only to communicate failure of deconstruction and does not return
+    /// a value.
     type DeconstructFuture: Future<Output = DeconstructResult<Self>> + Send + 'static;
 
+    /// Moves out of the `Kind` and deconstructs on to the provided channel.
+    /// As with `construct`, this method should return immediately.
     fn deconstruct<C: Channel<Self::DeconstructItem, Self::ConstructItem>>(
         self,
         channel: C,
     ) -> Self::DeconstructFuture;
 }
 
+/// An erased representation of any serializable type used in communication
+/// by `Kind`.
 pub(crate) trait SerdeAny: erased_serde::Serialize + mopa::Any + Send {}
 
 mopafy!(SerdeAny);
