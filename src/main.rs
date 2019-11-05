3d64ad8ae10d1a340087f57d0c0a63573995780f
Izzy Swart <zenerboson@gmail.com>
2019-11-05 11:15:18 -0800

    fix supertraits when generic parameters exist on subtrait

--- src/main.rs
+++ src/main.rs
@@ -15,37 +15,37 @@ pub trait Supertrait {
 }
 
 #[object]
-pub trait Test<T: Kind> {
-    fn test(&self, hello: String, t: T) -> Future<u32>;
+pub trait Test<T: Kind>: Supertrait {
+    fn test(&self, hello: String) -> Future<u32>;
 }
 
 impl Supertrait for Shim {
-    fn super_test(&self, hello: String) -> Future<u32> {
-        Box::pin(async move { (hello.len() + 1) as u32 })
+    fn super_test(&self, _: String) -> Future<u32> {
+        Box::pin(async move { 2u32 })
     }
 }
 
 struct Shim;
 
 impl Test<u32> for Shim {
-    fn test(&self, hello: String, _: u32) -> Future<u32> {
+    fn test(&self, hello: String) -> Future<u32> {
         Box::pin(async move { hello.len() as u32 })
     }
 }
 
 fn main() {
     let trait_object = Box::new(Shim) as Box<dyn Test<u32>>;
-    let method_index = trait_object.by_name("test").unwrap();
     let supertraits = trait_object.supertraits();
     println!("{:?}", supertraits);
-    //let upcast_object: Upcasted<dyn Supertrait> =
-    //    *Box::<dyn Any + Send>::downcast(trait_object.upcast(supertraits[0]).unwrap()).unwrap();
+    let upcast_object: Upcasted<dyn Supertrait> =
+     *Box::<dyn Any + Send>::downcast(trait_object.upcast(supertraits[0]).unwrap()).unwrap();
+    let method_index = upcast_object.by_name("super_test").unwrap();
     println!(
         "{}",
         block_on(
             *Box::<dyn Any + Send>::downcast::<Future<u32>>(
-                trait_object
-                    .call(method_index, vec![Box::new("four".to_owned()), Box::new(0u32)])
+                upcast_object
+                    .call(method_index, vec![Box::new("four".to_owned())])
                     .unwrap()
             )
             .unwrap()
