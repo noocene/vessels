66f897754c6cf3668ed75351651fa6d3c78f997b
Izzy Swart <zenerboson@gmail.com>
2019-11-06 11:11:43 -0800

    gate core on export macro

--- derive/src/export.rs
+++ derive/src/export.rs
@@ -8,6 +8,8 @@ pub fn build(_: TokenStream) -> TokenStream {
         const EXPORT_ITEMS_: () = {
             #[cfg(not(target_arch = "wasm32"))]
             compile_error!("vessel must be compiled to wasm");
+            #[cfg(feature = "core")]
+            compile_error!("vessel cannot be compiled against core");
         };
     }
 }
