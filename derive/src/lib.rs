9d15e9c48c4a07e77ee7e0669f91a4985e48ff97
Izzy Swart <zenerboson@gmail.com>
2019-11-06 11:01:03 -0800

    add export macro boilerplate

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,3 +1,4 @@
+mod export;
 mod kind;
 mod object;
 
@@ -5,16 +6,21 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::ToTokens;
-use syn::{parse, ItemTrait};
+use syn::parse;
 use synstructure::decl_derive;
 
 decl_derive!([Kind, attributes(kind)] => kind::derive);
 
 #[proc_macro_attribute]
 pub fn object(attribute: TokenStream, item: TokenStream) -> TokenStream {
-    let mut item = parse::<ItemTrait>(item.clone()).unwrap_or_else(|_| panic!("expected trait"));
+    let mut item = parse(item.clone()).unwrap_or_else(|_| panic!("expected trait"));
     let extension = object::build(attribute.into(), &mut item);
     let mut item = item.into_token_stream();
     item.extend(extension);
     item.into()
+}
+
+#[proc_macro]
+pub fn export(item: TokenStream) -> TokenStream {
+    export::build(item.into()).into()
 }
