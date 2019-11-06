0f84d45dfeb13aa9909e0dfd5939f0c790f3c8fe
Izzy Swart <zenerboson@gmail.com>
2019-11-06 11:21:53 -0800

    ensure produced type has Kind

--- derive/src/export.rs
+++ derive/src/export.rs
@@ -1,9 +1,17 @@
 use proc_macro2::TokenStream;
 use quote::quote;
 
-pub fn build(_: TokenStream) -> TokenStream {
+pub fn build(block: TokenStream) -> TokenStream {
     quote! {
-        fn main() {}
+        fn main() {
+            let _export_initializer: fn() -> _ = || {
+                #block
+            };
+            let _test_shims: () = {
+                pub struct export<T: ::vessels::Kind>(fn() -> T);
+                export(_export_initializer);
+            };
+        }
 
         const EXPORT_ITEMS_: () = {
             #[cfg(not(target_arch = "wasm32"))]
