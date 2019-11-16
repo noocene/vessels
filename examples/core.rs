3242d2cb4ffefc3f3439802de55000868ab79622
Izzy Swart <zenerboson@gmail.com>
2019-11-15 18:56:51 -0800

    start network infrastructure

--- examples/core.rs
+++
@@ -1,8 +0,0 @@
-use vessels::{core::{hal::crypto::Rng, Executor, executor::Spawn}, core, log};
-
-fn main() {
-    core::<dyn Executor>().unwrap().run(async move {
-        let mut rng = Rng::new().unwrap();
-        log!("{:?}", rng.bytes(16).await);
-    });
-}
