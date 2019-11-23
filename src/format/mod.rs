878bea139f90a45e3ef9a5b333208013600182cf
Izzy Swart <zenerboson@gmail.com>
2019-11-23 15:54:48 -0800

    receive kind over network

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -183,7 +183,7 @@ where
                                 .or_else(move |(e, item)| {
                                     let context = ct.clone();
                                     let message = format!("{}", e);
-                                    let mut data = message.split(" ");
+                                    let mut data = message.split_whitespace();
                                     if data.next() == Some("ASYNC_WAIT") {
                                         if let Some(data) = data.next() {
                                             return context.wait_for(data.to_owned()).then(
