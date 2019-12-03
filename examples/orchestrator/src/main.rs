14c3594c9959b92e18c60d796a77f9102dd76891
Izzy Swart <zenerboson@gmail.com>
2019-12-02 17:12:43 -0800

    singleton register

--- examples/orchestrator/src/main.rs
+++ examples/orchestrator/src/main.rs
@@ -2,7 +2,8 @@ use vessels::{
     core::{
         data::Resource,
         orchestrator::{Module, Orchestrator},
-        run, Core,
+        run, Core, register,
+        hal::crypto::Hasher
     },
     kind::Future,
     log,
@@ -22,10 +23,14 @@ pub fn main() {
     let binary = read("../../target/wasm32-unknown-unknown/debug/test_vessel.wasm").unwrap();
     run(async move {
         let orchestrator = Orchestrator::new().unwrap();
+        register(|| Hasher::new().unwrap());
         let mut core = Core::new();
         core.register(|| Box::new(Tester) as Box<dyn test_vessel::Test>);
         let data: String = orchestrator
-            .instantiate(Resource::new(Module::new(binary)).await.unwrap(), core.into_handle())
+            .instantiate(
+                Resource::new(Module::new(binary)).await.unwrap(),
+                core.into_handle(),
+            )
             .await
             .unwrap();
         log!("{}", data);
