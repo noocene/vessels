db1d14ce8b9fee9d48aef7c73879e8c6e7687a12
Izzy Swart <zenerboson@gmail.com>
2019-12-02 23:06:04 -0800

    reactive lists

--- src/replicate/collections/list.rs
+++ src/replicate/collections/list.rs
@@ -1,5 +1,14 @@
-use crate::{kind::Future, object, replicate::Share, Kind};
-use futures::future::ready;
+use crate::{
+    kind::{Future, Stream},
+    object,
+    replicate::{Reactive, Share},
+    Kind,
+};
+
+use futures::{
+    channel::mpsc::{unbounded, UnboundedSender},
+    future::ready,
+};
 
 #[object]
 pub trait List<T: Kind + Share> {
@@ -38,5 +47,64 @@ impl<T: Kind + Share> List<T> for Vec<T> {
             removed = false;
         }
         Box::pin(ready(removed))
+    }
+}
+
+#[derive(Kind)]
+pub enum Mutation<T: Kind + Share> {
+    Pop,
+    Push(T),
+    Remove(u32),
+}
+
+impl<T: Kind + Share> Share for Mutation<T> {
+    fn share(&self) -> Self {
+        match self {
+            Mutation::Pop => Mutation::Pop,
+            Mutation::Push(item) => Mutation::Push(item.share()),
+            Mutation::Remove(index) => Mutation::Remove(*index),
+        }
+    }
+}
+
+struct Observed<T: Kind + Share>(Box<dyn List<T>>, UnboundedSender<Mutation<T>>);
+
+impl<T: Kind + Share> Observed<T> {
+    fn new(item: Box<dyn List<T>>) -> (Self, Stream<Mutation<T>>) {
+        let (sender, receiver) = unbounded();
+        (Observed(item, sender), Box::pin(receiver))
+    }
+}
+
+impl<T: Kind + Share> List<T> for Observed<T> {
+    fn len(&self) -> Future<u32> {
+        self.0.len()
+    }
+    fn is_empty(&self) -> Future<bool> {
+        self.0.is_empty()
+    }
+    fn index(&self, index: u32) -> Future<Option<T>> {
+        self.0.index(index)
+    }
+    fn push(&mut self, item: T) -> Future<()> {
+        self.1.start_send(Mutation::Push(item.share())).unwrap();
+        self.0.push(item)
+    }
+    fn pop(&mut self) -> Future<Option<T>> {
+        self.1.start_send(Mutation::Pop).unwrap();
+        self.0.pop()
+    }
+    fn remove(&mut self, index: u32) -> Future<bool> {
+        self.1.start_send(Mutation::Remove(index)).unwrap();
+        self.0.remove(index)
+    }
+}
+
+impl<T: Kind + Share> Reactive for Box<dyn List<T>> {
+    type Mutation = Mutation<T>;
+
+    fn react(self) -> (Self, Stream<Self::Mutation>) {
+        let (item, stream) = Observed::new(self);
+        (Box::new(item), stream)
     }
 }
