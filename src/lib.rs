91f3d05aa2f1acbf2903e81f3c7b2450b5232669
Izzy Swart <zenerboson@gmail.com>
2020-01-30 08:48:13 -0800

    adjust bounds

--- src/lib.rs
+++ src/lib.rs
@@ -37,22 +37,22 @@ pub trait Pass<'a, P: Protocol<'a, Self>>: Spawn<'a, P> + Join<'a, P> {}
 
 impl<'a, P: Protocol<'a, T>, T: Spawn<'a, P> + Join<'a, P>> Pass<'a, P> for T {}
 
-pub trait Channel<T, U, S: Context>:
+pub trait Channel<T, U, S: ?Sized + Context>:
     Stream<Item = T> + Sink<U, Error = S::SinkError> + DerefMut<Target = S>
 {
 }
 
-pub trait Context: Sized {
+pub trait Context {
     type Handle;
     type SinkError;
 }
 
-pub trait Channels<Unravel, Coalesce>: Context + Sized {
+pub trait Channels<Unravel, Coalesce>: Context {
     type Unravel: Channel<Coalesce, Unravel, Self>;
     type Coalesce: Channel<Unravel, Coalesce, Self>;
 }
 
-pub trait Protocol<'a, C: Context>: Sized {
+pub trait Protocol<'a, C: ?Sized>: Sized {
     type Unravel;
     type UnravelFuture: TryFuture<Ok = ()>;
     type Coalesce;
@@ -65,4 +65,26 @@ pub trait Protocol<'a, C: Context>: Sized {
     fn coalesce(channel: &'a mut C::Coalesce) -> Self::CoalesceFuture
     where
         C: Channels<Self::Unravel, Self::Coalesce> + 'static;
+}
+
+pub enum DirectorError<T, U> {
+    Director(T),
+    Protocol(U),
+}
+
+trait Director<'a, P: Protocol<'a, Self::Context>, Transport> {
+    type Context: Channels<P::Unravel, P::Coalesce>;
+    type UnravelError;
+    type Unravel: TryFuture<
+        Ok = (),
+        Error = DirectorError<Self::UnravelError, <P::UnravelFuture as TryFuture>::Error>,
+    >;
+    type CoalesceError;
+    type Coalesce: TryFuture<
+        Ok = P,
+        Error = DirectorError<Self::CoalesceError, <P::CoalesceFuture as TryFuture>::Error>,
+    >;
+
+    fn unravel(protocol: P, transport: Transport) -> Self::Unravel;
+    fn coalesce(transport: Transport) -> Self::Coalesce;
 }
